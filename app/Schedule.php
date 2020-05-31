733438a32d6244530c930c92d7634d200e9cb4d3
Alexey Fedulov <alexey.fedulov98@gmail.com>
2020-05-31 12:59:51 +0300

    Update fillable

--- app/Schedule.php
+++ app/Schedule.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class Schedule extends Model
 {
-    //
+    protected $fillable = [
+        'group_id',
+        'week',
+        'type',
+        'first_lesson_name',
+        'first_lesson_teacher',
+        'second_lesson_name',
+        'second_lesson_teacher',
+        'third_lesson_name',
+        'third_lesson_teacher',
+        'fouth_lesson_name',
+        'fouth_lesson_teacher',
+        'fifth_lesson_name',
+        'fifth_lesson_teacher',
+        'sixth_lesson_name',
+        'sixth_lesson_teacher',
+        'seventh_lesson_name',
+        'seventh_lesson_teacher',
+        'eight_lesson_name',
+        'eight_lesson_teacher',
+    ];
 }
